75197c6d008452ed7e58d77385deda04396785a1
Suraj Mourya <mouryasuraj6266444@gmail.com>
2026-03-05 02:07:51 +0530

    feature: logout functionality add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,32 @@
 import React, { useEffect } from 'react'
 import { handleVerifyToken } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import axiosInstance from '../services/axios'
+import { showMessage } from '../utils/constants/showMessage'
+import { useNavigate } from 'react-router-dom'
+import { setIsAuthChecked, setIsAuthenticated, setUser } from '../features/auth/slices/authSlice'
 
 const Home = () => {
-    useEffect(() => {
-        const verify = async () => {
-            try {
-                const response = await handleVerifyToken()
-            } catch (error) {
-                console.log("Token verification failed")
-            } finally {
-            }
+
+    const {user} = useSelector(store => store.auth)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const handleLogout = async () =>{
+        const res = await axiosInstance.get("/auth/logout")
+        if(res.status===200){
+            showMessage("success", "Logged out successfully")
+            navigate("/auth/login")
+            dispatch(setUser(null))
+            dispatch(setIsAuthenticated(false))
         }
-        verify()
-    }, [])
+    }
+
     return (
-        <div>Home</div>
+        <div className='flex items-center gap-10 m-4'>
+            Welcome, {user.email}
+            <button onClick={handleLogout} className='px-4 py-2 rounded-lg border cursor-pointer hover:bg-gray-100'>Logout</button>
+        </div>
     )
 }
 
--- src/features/auth/hooks/useLoginForm.js
+++ src/features/auth/hooks/useLoginForm.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { login } from "../slices/authThunks.js";
